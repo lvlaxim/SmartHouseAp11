aede4aa959e9892a9f3b680b71eb61da19e83c61
lvlaxim <lvlaxim@yandex.ru>
2017-03-19 18:00:23 +0300

    Добавлено включение вентиляции в сан. узле после RESTROOM_VENT_START миллисекунд и её отключение вместе со светом.

--- SmartHouseAp11/SmartHouseAp11.ino
+++ SmartHouseAp11/SmartHouseAp11.ino
@@ -1,7 +1,9 @@
 #include "Button.h"
 
-#define RESTROOM_LIGHT_FORGOTTEN 5000      // Время в миллисекундах, после прохождения которого считается, что свет в сан. узле забыли выключить.
+#define RESTROOM_LIGHT_FORGOTTEN 8000      // Время в миллисекундах, после прохождения которого считается, что свет в сан. узле забыли выключить.
+#define RESTROOM_VENT_START 4000           // Время, через которое после включения света в сан.узле, должна включиться вентиляция.
 
+#define RESTROOM_VENT 6                    // Вентиляция в сан. узле -- 6 пин
 #define RESTROOM_LIGHT 7                   // Свет в сан. узле -- 7 пин
 
 Button restroomSwitch(2); // Выключатель в сан. узле -- 2-й пин
@@ -17,10 +19,12 @@ void on_press() {
 void on_long_release() {
     Serial.print(String("button released after long time: ") + restroomSwitch.gap() + String(" milliseconds\n"));
     digitalWrite(RESTROOM_LIGHT, LOW);
+    digitalWrite(RESTROOM_VENT, LOW);
 }
 void on_long_press() {
     Serial.print(String("button pressed after long time: ") + restroomSwitch.gap() + String(" milliseconds\n"));
     digitalWrite(RESTROOM_LIGHT, HIGH);
+    //digitalWrite(RESTROOM_VENT, HIGH);
 }
 void on_bounced_release() {
     Serial.print(String("button released after a bounce: ") + restroomSwitch.gap() + String(" milliseconds\n"));
@@ -32,6 +36,7 @@ void on_bounced_press() {
 
 void setup() {
   pinMode(RESTROOM_LIGHT, OUTPUT);
+  pinMode(RESTROOM_VENT, OUTPUT);
   Serial.begin(9600);
   restroomSwitch.on_release(on_release);                      // on NBP (if not set nothing will be done)
   restroomSwitch.on_press(on_press);                          // on NBsP (if not set nothing will be done)
@@ -44,7 +49,14 @@ void setup() {
 void loop() {
   restroomSwitch.init();
   Serial.print(restroomSwitch.timePressed() + String(" milliseconds\n"));
-  if(restroomSwitch.timePressed() >= RESTROOM_LIGHT_FORGOTTEN) digitalWrite(RESTROOM_LIGHT, LOW);
+
+  if((restroomSwitch.timePressed() >= RESTROOM_VENT_START) && (restroomSwitch.timePressed() < RESTROOM_LIGHT_FORGOTTEN)) digitalWrite(RESTROOM_VENT, HIGH);
+  
+  if(restroomSwitch.timePressed() >= RESTROOM_LIGHT_FORGOTTEN){
+    digitalWrite(RESTROOM_LIGHT, LOW);
+    digitalWrite(RESTROOM_VENT, LOW);
+  }
+  
   
   delay(1000);
 }
