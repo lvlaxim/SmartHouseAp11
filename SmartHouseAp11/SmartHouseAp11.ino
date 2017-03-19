5a2d78723084307dc646f85facf8fd5af57e9bac
lvlaxim <lvlaxim@yandex.ru>
2017-03-19 20:34:01 +0300

    Убранны лишний код и комментарии.

--- SmartHouseAp11/SmartHouseAp11.ino
+++ SmartHouseAp11/SmartHouseAp11.ino
@@ -11,12 +11,6 @@ Button restroomSwitch(2); // Выключатель в сан. узле -- 2-й 
 
 // funtions that will be called whenever one of the XB(s)P's event occurs
 
-void on_release() {
-    Serial.print(String("button released immediately: ") + restroomSwitch.gap() + String(" milliseconds\n"));
-}
-void on_press() {
-    Serial.print(String("button pressed immediately: ") + restroomSwitch.gap() + String(" milliseconds\n"));
-}
 void on_long_release() {
     Serial.print(String("button released after long time: ") + restroomSwitch.gap() + String(" milliseconds\n"));
     digitalWrite(RESTROOM_LIGHT, LOW);
@@ -25,30 +19,18 @@ void on_long_press() {
     Serial.print(String("button pressed after long time: ") + restroomSwitch.gap() + String(" milliseconds\n"));
     digitalWrite(RESTROOM_LIGHT, HIGH);
 }
-void on_bounced_release() {
-    Serial.print(String("button released after a bounce: ") + restroomSwitch.gap() + String(" milliseconds\n"));
-}
-void on_bounced_press() {
-    Serial.print(String("button pressed after a bounce: ") + restroomSwitch.gap() + String(" milliseconds\n"));
-}
 
 
 void setup() {
   pinMode(RESTROOM_LIGHT, OUTPUT);
   pinMode(RESTROOM_VENT, OUTPUT);
   Serial.begin(9600);
-  restroomSwitch.on_release(on_release);                      // on NBP (if not set nothing will be done)
-  restroomSwitch.on_press(on_press);                          // on NBsP (if not set nothing will be done)
   restroomSwitch.on_long_press(on_long_press);                // on LBsP (if not set, on_press will be used for on_long_press events)
   restroomSwitch.on_long_release(on_long_release);            // on LBP (if not set, on_release will be used for on_long_release events)
-  restroomSwitch.on_bounced_release(on_bounced_release);      // on BBP (junk, if not set nothing will be done)
-  restroomSwitch.on_bounced_press(on_bounced_press);          // on BBsP (junk, if not set nothing will be done)
 }
 
 void loop() {
   restroomSwitch.init();
-  //Serial.print(String("Buttone is pressed ") + restroomSwitch.timePressed() + String(" milliseconds\n"));
-  //Serial.print(String("Buttone is released ") + restroomSwitch.timeReleased() + String(" milliseconds\n"));
 
   // Включаем вентиляцию в сан.узле если прошло время RESTROOM_VENT_START, но еще не настало время "забытия"
   if((restroomSwitch.timePressed() >= RESTROOM_VENT_START) && (restroomSwitch.timePressed() < RESTROOM_LIGHT_FORGOTTEN)) digitalWrite(RESTROOM_VENT, HIGH);
