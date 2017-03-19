842c5ec7e1232e2b759259c0a4041f9981f59d97
lvlaxim <lvlaxim@yandex.ru>
2017-03-19 20:28:41 +0300

    Добавлена отработка вентиляции после того как свет выключат после продолжительного включеня.

--- SmartHouseAp11/SmartHouseAp11.ino
+++ SmartHouseAp11/SmartHouseAp11.ino
@@ -1,10 +1,11 @@
 #include "Button.h"
 
-#define RESTROOM_LIGHT_FORGOTTEN 8000      // Время в миллисекундах, после прохождения которого считается, что свет в сан. узле забыли выключить.
-#define RESTROOM_VENT_START 4000           // Время, через которое после включения света в сан.узле, должна включиться вентиляция.
+#define RESTROOM_LIGHT_FORGOTTEN 8000       // Время в миллисекундах, после прохождения которого считается, что свет в сан. узле забыли выключить.
+#define RESTROOM_VENT_START 4000            // Время, через которое после включения света в сан.узле, должна включиться вентиляция.
+#define RESTROOM_VENT_AFTER 3000            // Время, через которое выключается вентиляция после выключения.
 
-#define RESTROOM_VENT 6                    // Вентиляция в сан. узле -- 6 пин
-#define RESTROOM_LIGHT 7                   // Свет в сан. узле -- 7 пин
+#define RESTROOM_VENT 6                     // Вентиляция в сан. узле -- 6 пин
+#define RESTROOM_LIGHT 7                    // Свет в сан. узле -- 7 пин
 
 Button restroomSwitch(2); // Выключатель в сан. узле -- 2-й пин
 
@@ -19,7 +20,6 @@ void on_press() {
 void on_long_release() {
     Serial.print(String("button released after long time: ") + restroomSwitch.gap() + String(" milliseconds\n"));
     digitalWrite(RESTROOM_LIGHT, LOW);
-    digitalWrite(RESTROOM_VENT, LOW);
 }
 void on_long_press() {
     Serial.print(String("button pressed after long time: ") + restroomSwitch.gap() + String(" milliseconds\n"));
@@ -47,8 +47,8 @@ void setup() {
 
 void loop() {
   restroomSwitch.init();
-  Serial.print(String("Buttone is pressed ") + restroomSwitch.timePressed() + String(" milliseconds\n"));
-  Serial.print(String("Buttone is released ") + restroomSwitch.timeReleased() + String(" milliseconds\n"));
+  //Serial.print(String("Buttone is pressed ") + restroomSwitch.timePressed() + String(" milliseconds\n"));
+  //Serial.print(String("Buttone is released ") + restroomSwitch.timeReleased() + String(" milliseconds\n"));
 
   // Включаем вентиляцию в сан.узле если прошло время RESTROOM_VENT_START, но еще не настало время "забытия"
   if((restroomSwitch.timePressed() >= RESTROOM_VENT_START) && (restroomSwitch.timePressed() < RESTROOM_LIGHT_FORGOTTEN)) digitalWrite(RESTROOM_VENT, HIGH);
@@ -58,7 +58,11 @@ void loop() {
     digitalWrite(RESTROOM_LIGHT, LOW);
     digitalWrite(RESTROOM_VENT, LOW);
   }
+
+  // Включенная вентиляция должна отработать еще RESTROOM_VENT_AFTER после выключения света
+  if((restroomSwitch.timeReleased() >= 1) && (restroomSwitch.timeReleased() >= RESTROOM_VENT_AFTER) && (restroomSwitch.gap() >= RESTROOM_VENT_START)){
+    digitalWrite(RESTROOM_VENT, LOW);
+  }
   
-  
-  delay(1000);
+  delay(500);
 }
